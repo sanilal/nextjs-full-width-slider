86dc0cfe8140255efb27e9821c5e1233f847f02c
Sanilal P Sathyan <sanilal@gmail.com>
2024-04-23 12:43:36 +0400

    react-slideshow-image and icons added

---
+++ src/app/components/Slideshow.jsx
@@ -0,0 +1,22 @@
+import { IoIosArrowDropleftCircle, IoIosArrowDroprightCircle } from "react-icons/io";
+import { Zoom } from "react-slideshow-image";
+import 'react-slideshow-image/dist/styles.css'
+
+export default function Slideshow() {
+const images = [
+    "1.webp",
+    "2.webp",
+    "3.webp",
+]
+
+const zoomInProperties = {
+    scale:1,
+    duration:5000,
+    transitionDuration:300,
+    infinity:true
+}
+
+  return (
+    <div>Slideshow</div>
+  )
+}
